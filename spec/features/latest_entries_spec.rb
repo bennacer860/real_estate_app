c4d82bb1fc715fc35476c528aaf572c2dc0f370a
bennacer <bennacer860@gmail.com>
2013-06-06 16:26:39 -0400

    pass the integration test

--- spec/features/latest_entries_spec.rb
+++ spec/features/latest_entries_spec.rb
@@ -3,6 +3,16 @@ require  'spec_helper'
 feature 'Latest 5 properties on homepage' do
 	
 	scenario 'show the latest 5 properties on homepage' do
+
+		# background do
+		p1 = create(:property,:title => 'Property 1', :created_at => 6.days.ago)
+		p2 = create(:property,:title => 'Property 2', :created_at => 5.days.ago)
+		p3 = create(:property,:title => 'Property 3', :created_at => 4.days.ago)
+		p4 = create(:property,:title => 'Property 4', :created_at => 3.days.ago)
+		p5 = create(:property,:title => 'Property 5', :created_at => 2.days.ago)
+		p6 = create(:property,:title => 'Property 6', :created_at => 1.day.ago)
+        # end
+
 		visit root_path
 	    expect(page).to have_content 'Property 6'
 	    expect(page).to have_content 'Property 5'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
+require 'factory_girl_rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -30,6 +31,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.include FactoryGirl::Syntax::Methods
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
