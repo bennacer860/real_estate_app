90e0a17b24f444c8d718f8dc6a256f7535945eea
bennacer <bennacer860@gmail.com>
2013-06-10 11:30:32 -0400

    create unit tests for property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,8 @@
 class Property < ActiveRecord::Base
   attr_accessible :city_name, :description, :price, :title
+  validates :city_name, :presence => true
+  validates :title, :presence => true
+  validates :price, :presence => true
+  validates :description, :presence => true
+
 end
